8ef33d6f28ddad666cc49a4a7886a9350fcc4eda
Emanuele <conte.emanuele@protonmail.com>
2023-07-11 14:49:30 +0200

    svolta verifica primo statement

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main (String[] args) {
+        int a = 15;
+        a += 5;
+        a -= 4;
+        int b = a;
+        b += 1;
 
+        boolean isOdd = true;
+        if (b % 2 == 0) {
+            isOdd = false;
+        }
+        System.out.println("Il numero è dispari: " + isOdd);
     }
 }
